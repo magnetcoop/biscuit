4c203544edf306a2e0be89d59f47609ea18d108f
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-06-03 16:09:36 +0200

    Prepare for next development version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject biscuit "1.0.0"
-  :description "Calculate message digests"
-  :url "https://github.com/henrygarner/biscuit"
+(defproject biscuit "1.1.0-SNAPSHOT"
+  :description "Calculate message digests."
+  :url "https://github.com/magnetcoop/biscuit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
