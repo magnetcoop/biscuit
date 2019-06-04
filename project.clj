02c5488577c9a487f7a73d6a7dc98f144a59713c
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-06-04 15:53:10 +0200

    [#4] Add infrastructure to publish our own version to Clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,16 @@
-(defproject biscuit "1.1.0-SNAPSHOT"
+(defproject magnet/biscuit "1.1.0-SNAPSHOT"
   :description "Calculate message digests."
   :url "https://github.com/magnetcoop/biscuit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
   :plugins [[lein-cljfmt "0.6.2"]
             [jonase/eastwood "0.3.4"]])
